26a38d62872f19ee892d57973c7cb819ad799dc1
pushpak <pushpak99311@gmail.com>
2022-07-18 17:58:13 +0530

    url not found validation added

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,6 +6,9 @@ const {createShortUrl, redirectUrl} = require('../controllers/urlController')
 router.post("/url/shorten", createShortUrl)
 router.get("/:urlCode", redirectUrl)
 
+router.all("/*", function (req, res){
+    res.status(404).send({status :false, message: "URL not found."})
+})
 
 
 module.exports = router
